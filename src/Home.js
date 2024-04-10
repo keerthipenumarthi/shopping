5326f73de014674007aede108ebfcf7b7ff6b4a7
keerthi <keerthipenumarthi@gmail.com>
2024-04-10 10:55:09 +0530

    added navigation

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import "./App";
+import "App.css";
+import "index.css";
+{
+  console.log("hello");
+}
+export default App;
